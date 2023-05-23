7722a54bdb36f44f1b368362473ee68a2c581ce5
exxsid <cortezleoanthony5@gmail.com>
2023-05-23 20:03:41 +0800

    added the header

--- authentication/home.php
+++ authentication/home.php
@@ -1,15 +1,18 @@
 <?php
 session_start();
 
-echo "Hello user " . $_SESSION['userId'];
-
 if (isset($_POST['logout'])) {
     session_destroy();
 
     header("Location: index.php");
 }
+
+include './views/partials/header.php';
 ?>
 
-<form action="" method="post">
-    <button type="submit" name='logout' class="btn btn-primary">Logout</button>
-</form>
+<body>
+    <?= "Hello user " . $_SESSION['userId']; ?>
+    <form action="" method="post">
+        <button type="submit" name='logout' class="btn btn-primary">Logout</button>
+    </form>
+</body>
