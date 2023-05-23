c5f15ff4758004d5cd9640dbf9d5bbb7039fb7a8
exxsid <cortezleoanthony5@gmail.com>
2023-05-23 20:49:43 +0800

    edit home page to connect to firestore

--- authentication/home.php
+++ authentication/home.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+require_once './utils/db.php';
+
 if (isset($_POST['logout'])) {
     session_destroy();
 
@@ -11,7 +13,11 @@ include './views/partials/header.php';
 ?>
 
 <body>
-    <?= "Hello user " . $_SESSION['userId']; ?>
+    <?php
+    $user = $firestore->database()->collection('users2')->document($_SESSION['userId'])->snapshot();
+    $username = $user['username'];
+    ?>
+    <?= "Hello user " .  $username ?>
     <form action="" method="post">
         <button type="submit" name='logout' class="btn btn-primary">Logout</button>
     </form>
--- authentication/utils/db.php
+++ authentication/utils/db.php
@@ -3,9 +3,9 @@
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
 
-require_once '../vendor/autoload.php';
+require_once 'vendor\autoload.php';
 
-$keyPath = "..\key\infoman2-g2-2bd13-firebase-adminsdk-c7b51-c3abefd114.json";
+$keyPath = "key\infoman2-g2-2bd13-firebase-adminsdk-c7b51-c3abefd114.json";
 
 $serviceAccJSON = file_get_contents($keyPath);
 
