85876f7898a7abf0e7786dc5b8a3186b462b440a
exxsid <cortezleoanthony5@gmail.com>
2023-05-23 20:38:48 +0800

    separated the db utils

--- authentication/handler/signinhandler.php
+++ authentication/handler/signinhandler.php
@@ -3,20 +3,8 @@ session_start();
 
 use Kreait\Firebase\Exception\Auth\InvalidPassword;
 use Kreait\Firebase\Exception\Auth\UserNotFound;
-use Kreait\Firebase\Factory;
-use Kreait\Firebase\ServiceAccount;
 
-require_once './vendor/autoload.php';
-
-$keyPath = "key\infoman2-g2-2bd13-firebase-adminsdk-c7b51-c3abefd114.json";
-
-$serviceAccJSON = file_get_contents($keyPath);
-
-$serviceAcc = ServiceAccount::fromValue($serviceAccJSON);
-
-$factory = (new Factory)->withServiceAccount($serviceAcc);
-
-$auth = $factory->createAuth();
+include_once '../utils/db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
@@ -27,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['userId'] = $signinResult->firebaseUserId();
         $_SESSION['email'] = $email;
 
-        header("Location: home.php");
+        header("Location: ../home.php");
     } catch (UserNotFound $unf) {
         header("Location: signin.php?error=" . $unf->getMessage());
     } catch (InvalidPassword $ip) {
--- authentication/handler/signuphandler.php
+++ authentication/handler/signuphandler.php
@@ -1,21 +1,6 @@
 <?php
 
-use Kreait\Firebase\Factory;
-use Kreait\Firebase\ServiceAccount;
-
-require_once './vendor/autoload.php';
-
-$keyPath = "key\infoman2-g2-2bd13-firebase-adminsdk-c7b51-c3abefd114.json";
-
-$serviceAccJSON = file_get_contents($keyPath);
-
-$serviceAcc = ServiceAccount::fromValue($serviceAccJSON);
-
-$factory = (new Factory)->withServiceAccount($serviceAcc);
-
-$auth = $factory->createAuth();
-
-$firestore = $factory->createFirestore();
+include_once '../utils/db.php';
 
 
 
