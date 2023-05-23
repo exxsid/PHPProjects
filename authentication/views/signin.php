a5f75b06d09a9b48340f34624405daaf1a99553f
exxsid <cortezleoanthony5@gmail.com>
2023-05-23 20:25:21 +0800

    organized the files

--- authentication/views/signin.php
+++ authentication/views/signin.php
@@ -1,6 +1,6 @@
 <?php
-require_once './vendor/autoload.php';
-include './views/partials/header.php';
+require_once '../vendor/autoload.php';
+include './partials/header.php';
 ?>
 <?php
 if (isset($_GET['error'])) {
@@ -14,7 +14,7 @@ if (isset($_GET['error'])) {
 
 <div class="container">
     <h1>Sign In</h1>
-    <form action="signinhandler.php" method="post" class="m-5">
+    <form action="../handler/signinhandler.php" method="post" class="m-5">
         <div class="mb-3">
             <label for="" class="form-label">Email</label>
             <input type="text" name="email" class="form-control">
@@ -29,7 +29,7 @@ if (isset($_GET['error'])) {
             <button type="submit" class="btn btn-primary">Sign In</button>
         </div>
         <div class="">
-            <a href="index.php">Dont't have an account yet?</a>
+            <a href="../index.php">Dont't have an account yet?</a>
         </div>
     </form>
 </div>
