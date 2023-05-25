fcfbf6fdb4e8005695b2b2342cd952c925f11f0a
exxsid <cortezleoanthony5@gmail.com>
2023-05-25 15:32:34 +0800

    edited signuphandler for checking password

--- 2212573-InfoMan2/src/handler/signuphandler.php
+++ 2212573-InfoMan2/src/handler/signuphandler.php
@@ -6,29 +6,28 @@ if (!isset($_SESSION)) {
 
 include_once $_SERVER['DOCUMENT_ROOT'] . '\src\utils\db.php';
 
-
-
-$newUser = [
-    'username' => $_POST['username'],
-    'email' => $_POST['email'],
-    'password' => $_POST['password'],
-];
-if ($_POST['password'] != $_POST['confirmPass']) {
-    header("Location: ../index.php?error=Password is not the same");
-}
-
-try {
-    $user = $auth->createUser($newUser);
-
-    $firestore->database()->collection('users')->document($user->uid)->set([
+if ($_POST['password'] === $_POST['confirmPass']) {
+    $newUser = [
         'username' => $_POST['username'],
         'email' => $_POST['email'],
-        'firstname' => $_POST['firstname'],
-        'lastname' => $_POST['lastname'],
-        'avatar' => 'https://img.mensxp.com/media/content/2015/Jan/kindsoffacebookprofilephotoswearesickofseeing1_1420801169.jpg',
-    ]);
-} catch (Exception $e) {
-    header("Location: ../index.php?error=" . $e->getMessage());
-}
+        'password' => $_POST['password'],
+    ];
 
-header('Location: ../views/signin.php');
+    try {
+        $user = $auth->createUser($newUser);
+
+        $firestore->database()->collection('users')->document($user->uid)->set([
+            'username' => $_POST['username'],
+            'email' => $_POST['email'],
+            'firstname' => $_POST['firstname'],
+            'lastname' => $_POST['lastname'],
+            'avatar' => 'https://img.mensxp.com/media/content/2015/Jan/kindsoffacebookprofilephotoswearesickofseeing1_1420801169.jpg',
+        ]);
+    } catch (Exception $e) {
+        header("Location: ../index.php?error=" . $e->getMessage());
+    }
+
+    header('Location: ../views/signin.php');
+} else {
+    header("Location: ../../index.php?error=Password is not the same");
+}
