f2a8bb5b72bf05e17de8c0b92e228c8505acaed3
exxsid <cortezleoanthony5@gmail.com>
2023-05-26 06:50:12 +0800

    added post comment and like

--- 2212573-InfoMan2/functions/index.js
+++ 2212573-InfoMan2/functions/index.js
@@ -7,7 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
@@ -17,3 +17,59 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.commentsCounter = functions.firestore
+  .document("post_comments/{commentId}")
+  .onCreate((snapshot, context) => {
+    const postId = snapshot.data().post_id;
+
+    //query post_comments collection and fetch all
+    //whose post_id = postId
+    return admin
+      .firestore()
+      .collection("post_comments")
+      .where("post_id", "==", postId)
+      .get()
+      .then((querySnapshot) => {
+        const commentsCount = querySnapshot.size;
+        //update the post comment count
+        return admin
+          .firestore()
+          .collection("posts")
+          .doc(postId)
+          .update({ comment_count: commentsCount });
+      })
+      .catch((error) => {
+        console.error("Error updating comment count: ", error);
+      });
+  });
+
+exports.likesCounter = functions.firestore
+  .document("post_likes/{likeId}")
+  .onCreate((snapshot, context) => {
+    const postId = snapshot.data().post_id;
+
+    //query post_comments collection and fetch all
+    //whose post_id = postId
+    return admin
+      .firestore()
+      .collection("post_likes")
+      .where("post_id", "==", postId)
+      .get()
+      .then((querySnapshot) => {
+        const likeCount = querySnapshot.size;
+        //update the post comment count
+        return admin
+          .firestore()
+          .collection("posts")
+          .doc(postId)
+          .update({ like_count: likeCount });
+      })
+      .catch((error) => {
+        console.error("Error updating like count: ", error);
+      });
+  });
