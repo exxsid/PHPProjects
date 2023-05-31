e480c4374a8addb4be47c5d9ea862f746fed19e9
exxsid <cortezleoanthony5@gmail.com>
2023-05-31 09:58:38 +0800

    di ko alam

--- authentication/utils/db.php
+++ authentication/utils/db.php
@@ -3,6 +3,8 @@
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
 
+putenv("FIRESTORE_EMULATOR_HOST=127.0.0.1:8080");
+
 require_once 'vendor\autoload.php';
 
 $keyPath = "key\infoman2-g2-2bd13-firebase-adminsdk-c7b51-c3abefd114.json";
--- post-firebase/index.php
+++ post-firebase/index.php
@@ -1,11 +1,12 @@
 <?php
 
 require 'vendor\autoload.php';
-putenv('FIRESTORE_EMULATOR_HOST=localhost:8028');
+// putenv('FIRESTORE_EMULATOR_HOST=localhost:8028');
 
 use Google\Cloud\Firestore\FirestoreClient;
 
 $db = new FirestoreClient([
+    'keyFilePath' => 'keys\infoman2-g2-2bd13-firebase-adminsdk-c7b51-c3abefd114.json',
     "projectId" => "infoman2-g2-2bd13"
 ]);
 
